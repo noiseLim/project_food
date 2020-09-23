a9a388e0918cb7e2201197bfe5a322360dd54775
noiseLIm <noiselim@gmail.com>
2020-09-23 16:45:53 +0300

    create new project'Food'.add first script 'new tabs'

---
+++ js/script.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', () => {
+    
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach (item => {
+            // item.style.display = 'none'; //с использованием inline стилей
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        // tabsContent[i].style.display = 'block'; //с использованием inline стилей
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+    });
+});
